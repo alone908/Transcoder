bddc8be34367de33bf016655ccd64e03dcefcd61
Chung Jun <alone908@hotmail.com>
2017-09-30 19:27:49 +0800

    separate main rule and sub rule

--- js/rm_rulelist.js
+++ js/rm_rulelist.js
@@ -3,181 +3,199 @@ var defaultRuleSetID = 1;
 
 get_rule_list();
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $('#page-wrapper').css('width', ($(document).width()-350).toString()+'px' );
+    $('#page-wrapper').css('width', ($(document).width() - 350).toString() + 'px');
 
-  $('#rule-list-table > tbody > tr').on('click',function(e){ select_rule(e,$(this)); })
+    $('#rule-list-table-main > tbody > tr').on('click', function (e) {
+        select_rule(e, $(this));
+    })
+    $('#rule-list-table-sub > tbody > tr').on('click', function (e) {
+        select_rule(e, $(this));
+    })
 
-  $('.del_rule_btn').on('click',function(e){ $('#del_rule').data('rulesetid',$(this).data('rulesetid')); })
-  $('#del_rule').on('click',function(e){ del_rule($(this).data('rulesetid')); })
+    $('.del_rule_btn').on('click', function (e) {
+        $('#del_rule').data('rulesetid', $(this).data('rulesetid'));
+    })
+    $('#del_rule').on('click', function (e) {
+        del_rule($(this).data('rulesetid'));
+    })
 
-  $('.clone_rule_btn').on('click',function(e){
-    check_clone_rulename($(this).data('rulesetid'));
-    $('#clone_rule').data('rulesetid',$(this).data('rulesetid'));
-  })
-  $('#clone_rule').on('click',function(e){ clone_rule($(this).data('rulesetid')); })
+    $('.clone_rule_btn').on('click', function (e) {
+        check_clone_rulename($(this).data('rulesetid'));
+        $('#clone_rule').data('rulesetid', $(this).data('rulesetid'));
+    })
+    $('#clone_rule').on('click', function (e) {
+        clone_rule($(this).data('rulesetid'));
+    })
 
-  $('.edit_rule_btn').on('click',function(e){
-    $('#rule_name').val(ruleList[$(this).data('rulesetid')].RuleName);
-    $('#save_rule_name').data('rulesetid',$(this).data('rulesetid'));
-  })
-  $('#save_rule_name').on('click',function(e){ edit_rule_name($(this).data('rulesetid')); })
+    $('.edit_rule_btn').on('click', function (e) {
+        $('#rule_name').val(ruleList[$(this).data('rulesetid')].RuleName);
+        $('#save_rule_name').data('rulesetid', $(this).data('rulesetid'));
+    })
+    $('#save_rule_name').on('click', function (e) {
+        edit_rule_name($(this).data('rulesetid'));
+    })
 
-  $('#add_rule').on('click',function(e){ add_rule() });
+    $('#add_rule').on('click', function (e) {
+        add_rule()
+    });
 
-  $('#mainrule_radio_text').on('click',function(e){
-      $('input[value=MainRule]').prop('checked',true);
-      $('#mainrule_op').css('display','block');
-      $('#subrule_op').css('display','none');
-  })
+    $('#mainrule_radio_text').on('click', function (e) {
+        $('input[value=MainRule]').prop('checked', true);
+        $('#mainrule_op').css('display', 'block');
+        $('#subrule_op').css('display', 'none');
+    })
 
-  $('#subrule_radio_text').on('click',function(e){
-    $('input[value=SubRule]').prop('checked',true);
-    $('#mainrule_op').css('display','none');
-    $('#subrule_op').css('display','block');
-  })
+    $('#subrule_radio_text').on('click', function (e) {
+        $('input[value=SubRule]').prop('checked', true);
+        $('#mainrule_op').css('display', 'none');
+        $('#subrule_op').css('display', 'block');
+    })
 
-  $('input[name=ruletype]').on('change',function(e){
-    if($('input[name=ruletype]:checked').val() === 'MainRule'){
-      $('#mainrule_op').css('display','block');
-      $('#subrule_op').css('display','none');
-    }else if ($('input[name=ruletype]:checked').val() === 'SubRule') {
-      $('#mainrule_op').css('display','none');
-      $('#subrule_op').css('display','block');
-    }
-  })
+    $('input[name=ruletype]').on('change', function (e) {
+        if ($('input[name=ruletype]:checked').val() === 'MainRule') {
+            $('#mainrule_op').css('display', 'block');
+            $('#subrule_op').css('display', 'none');
+        } else if ($('input[name=ruletype]:checked').val() === 'SubRule') {
+            $('#mainrule_op').css('display', 'none');
+            $('#subrule_op').css('display', 'block');
+        }
+    })
 
 })
 
-function get_rule_list(){
-  $.ajax({
-    type: 'POST',
-    url: "appphp/TranscodeRule.php",
-    data: {op:'get_rule_list'},
-    dataType: "json",
-    success: function (data) {
-      ruleList = data.ruleList;
-    },
-    error: function(requestObject, error, errorThrown) {
-            $('#ajax_err').css('display','block');
-    }
-  });
-}
-
-function select_rule(e,ele){
-
-  var RuleSetID = ele.data('rulesetid');
-  currentRulesetID = RuleSetID;
-
-  $('#rule-list-table > tbody > tr').removeClass('info');
-  ele.addClass('info');
-
-  $('#rule-info').html('');
-  for(var key in ruleList[RuleSetID]){
-    $('#rule-info').append('<span style="font-size:18px">'+key+' : '+ruleList[RuleSetID][key]+'</span><br>');
-  }
-
-  $('#rule-title-li').text('Rule List - '+ruleList[RuleSetID]['RuleName']);
-  $('#rm_rulelist_href').attr('href','rm_rulelist.php?rulesetid='+RuleSetID);
-  $('#rm_ruleeditor_href').attr('href','rm_ruleeditor.php?rulesetid='+RuleSetID);
-  $('#rm_advanceeditor_href').attr('href','rm_advanceeditor.php?rulesetid='+RuleSetID);
-  $('#rm_rulebranch_href').attr('href','rm_rulebranch.php?rulesetid='+RuleSetID);
-  $('#rm_brancheditor_href').attr('href','rm_brancheditor.php?rulesetid='+RuleSetID);
-  $('#rm_preference_href').attr('href','rm_preference.php?rulesetid='+RuleSetID);
-}
-
-function add_rule(){
-  $('#loader').css('display','block');
-  $.ajax({
-    type: 'POST',
-    url: "appphp/rm_rulelist_backend.php",
-    data: {op:'add_rule',
-           rule_name:$('#name_of_rule').val(),
-           rule_type:$('input[name=ruletype]:checked').val(),
-           lines_in_head:$('#lines_in_head').val(),
-           lines_in_body:$('#lines_in_body').val(),
-           lines_in_rule:$('#lines_in_rule').val()},
-    dataType: "json",
-    success: function (data) {
-      window.location = 'rm_rulelist.php';
-    },
-    error: function(requestObject, error, errorThrown) {
-            $('#loader').css('display','none');
-            $('#ajax_err').css('display','block');
-    }
-  });
-}
-
-function del_rule(rulesetid){
-  $('#loader').css('display','block');
-  $.ajax({
-    type: 'POST',
-    url: "appphp/rm_rulelist_backend.php",
-    data: {op:'del_rule',rulesetid:rulesetid},
-    dataType: "json",
-    success: function (data) {
-      window.location = 'rm_rulelist.php';
-    },
-    error: function(requestObject, error, errorThrown) {
-            $('#loader').css('display','none');
-            $('#ajax_err').css('display','block');
-    }
-  });
-}
-
-function check_clone_rulename(rulesetid){
-  $('#loader').css('display','block');
-  $.ajax({
-    type: 'POST',
-    url: "appphp/rm_rulelist_backend.php",
-    data: {op:'check_clone_rulename',new_name:ruleList[rulesetid]['RuleName']+'[CLONE]'},
-    dataType: "json",
-    success: function (data) {
-      $('#new_rule_name').val(data.new_name);
-      $('#loader').css('display','none');
-    },
-    error: function(requestObject, error, errorThrown) {
-            $('#loader').css('display','none');
-            $('#ajax_err').css('display','block');
-    }
-  });
-}
-
-function clone_rule(rulesetid){
-  $('#loader').css('display','block');
-  $.ajax({
-    type: 'POST',
-    url: "appphp/rm_rulelist_backend.php",
-    data: {op:'clone_rule',rulesetid:rulesetid,rulename:$('#new_rule_name').val()},
-    dataType: "json",
-    success: function (data) {
-      window.location = 'rm_rulelist.php?rulesetid='+data.newrulesetid;
-    },
-    error: function(requestObject, error, errorThrown) {
-            $('#loader').css('display','none');
-            $('#ajax_err').css('display','block');
-    }
-  });
-}
-
-function edit_rule_name(rulesetid){
-  if($('#rule_name').val() === ''){
-    $('#edit_name_err').text('Rule name can not be left blank.');
-  }else {
-    $('#loader').css('display','block');
+function get_rule_list() {
     $.ajax({
-      type: 'POST',
-      url: "appphp/rm_rulelist_backend.php",
-      data: {op:'edit_rule_name',rulesetid:rulesetid,rulename:$('#rule_name').val()},
-      dataType: "json",
-      success: function (data) {
-        window.location = 'rm_rulelist.php?rulesetid='+currentRulesetID;
-      },
-      error: function(requestObject, error, errorThrown) {
-              $('#loader').css('display','none');
-              $('#ajax_err').css('display','block');
-      }
+        type: 'POST',
+        url: "appphp/TranscodeRule.php",
+        data: {op: 'get_rule_list'},
+        dataType: "json",
+        success: function (data) {
+            ruleList = data.ruleList;
+        },
+        error: function (requestObject, error, errorThrown) {
+            $('#ajax_err').css('display', 'block');
+        }
     });
-  }
+}
+
+function select_rule(e, ele) {
+
+    var RuleSetID = ele.data('rulesetid');
+    currentRulesetID = RuleSetID;
+
+    $('#rule-list-table-main > tbody > tr').removeClass('info');
+    $('#rule-list-table-sub > tbody > tr').removeClass('info');
+    ele.addClass('info');
+
+    $('#rule-info').html('');
+    for (var key in ruleList[RuleSetID]) {
+        $('#rule-info').append('<span style="font-size:18px">' + key + ' : ' + ruleList[RuleSetID][key] + '</span><br>');
+    }
+
+    $('#rule-title-li').text('Rule List - ' + ruleList[RuleSetID]['RuleName']);
+    $('#rm_rulelist_href').attr('href', 'rm_rulelist.php?rulesetid=' + RuleSetID);
+    $('#rm_ruleeditor_href').attr('href', 'rm_ruleeditor.php?rulesetid=' + RuleSetID);
+    $('#rm_advanceeditor_href').attr('href', 'rm_advanceeditor.php?rulesetid=' + RuleSetID);
+    $('#rm_rulebranch_href').attr('href', 'rm_rulebranch.php?rulesetid=' + RuleSetID);
+    $('#rm_brancheditor_href').attr('href', 'rm_brancheditor.php?rulesetid=' + RuleSetID);
+    $('#rm_preference_href').attr('href', 'rm_preference.php?rulesetid=' + RuleSetID);
+}
+
+function add_rule() {
+    $('#loader').css('display', 'block');
+    $.ajax({
+        type: 'POST',
+        url: "appphp/rm_rulelist_backend.php",
+        data: {
+            op: 'add_rule',
+            rule_name: $('#name_of_rule').val(),
+            rule_type: $('input[name=ruletype]:checked').val(),
+            lines_in_head: $('#lines_in_head').val(),
+            lines_in_body: $('#lines_in_body').val(),
+            lines_in_rule: $('#lines_in_rule').val()
+        },
+        dataType: "json",
+        success: function (data) {
+            window.location = 'rm_rulelist.php';
+        },
+        error: function (requestObject, error, errorThrown) {
+            $('#loader').css('display', 'none');
+            $('#ajax_err').css('display', 'block');
+        }
+    });
+}
+
+function del_rule(rulesetid) {
+    $('#loader').css('display', 'block');
+    $.ajax({
+        type: 'POST',
+        url: "appphp/rm_rulelist_backend.php",
+        data: {op: 'del_rule', rulesetid: rulesetid},
+        dataType: "json",
+        success: function (data) {
+            window.location = 'rm_rulelist.php';
+        },
+        error: function (requestObject, error, errorThrown) {
+            $('#loader').css('display', 'none');
+            $('#ajax_err').css('display', 'block');
+        }
+    });
+}
+
+function check_clone_rulename(rulesetid) {
+    $('#loader').css('display', 'block');
+    $.ajax({
+        type: 'POST',
+        url: "appphp/rm_rulelist_backend.php",
+        data: {op: 'check_clone_rulename', new_name: ruleList[rulesetid]['RuleName'] + '[CLONE]'},
+        dataType: "json",
+        success: function (data) {
+            $('#new_rule_name').val(data.new_name);
+            $('#loader').css('display', 'none');
+        },
+        error: function (requestObject, error, errorThrown) {
+            $('#loader').css('display', 'none');
+            $('#ajax_err').css('display', 'block');
+        }
+    });
+}
+
+function clone_rule(rulesetid) {
+    $('#loader').css('display', 'block');
+    $.ajax({
+        type: 'POST',
+        url: "appphp/rm_rulelist_backend.php",
+        data: {op: 'clone_rule', rulesetid: rulesetid, rulename: $('#new_rule_name').val()},
+        dataType: "json",
+        success: function (data) {
+            window.location = 'rm_rulelist.php?rulesetid=' + data.newrulesetid;
+        },
+        error: function (requestObject, error, errorThrown) {
+            $('#loader').css('display', 'none');
+            $('#ajax_err').css('display', 'block');
+        }
+    });
+}
+
+function edit_rule_name(rulesetid) {
+    if ($('#rule_name').val() === '') {
+        $('#edit_name_err').text('Rule name can not be left blank.');
+    } else {
+        $('#loader').css('display', 'block');
+        $.ajax({
+            type: 'POST',
+            url: "appphp/rm_rulelist_backend.php",
+            data: {op: 'edit_rule_name', rulesetid: rulesetid, rulename: $('#rule_name').val()},
+            dataType: "json",
+            success: function (data) {
+                window.location = 'rm_rulelist.php?rulesetid=' + currentRulesetID;
+            },
+            error: function (requestObject, error, errorThrown) {
+                $('#loader').css('display', 'none');
+                $('#ajax_err').css('display', 'block');
+            }
+        });
+    }
 }
--- rm_rulelist.php
+++ rm_rulelist.php
@@ -1,7 +1,7 @@
-<?php require_once 'appphp/sqldb.php';?>
-<?php include_once 'header.php';?>
-<?php include_once 'modals.php';?>
-<?php include_once 'appphp/rule_list_array.php';?>
+<?php require_once 'appphp/sqldb.php'; ?>
+<?php include_once 'header.php'; ?>
+<?php include_once 'modals.php'; ?>
+<?php include_once 'appphp/rule_list_array.php'; ?>
 <?php $page = 'rm_rulelist.php' ?>
 <?php $defaultRuleSetID = 1 ?>
 <?php $current_ruleset_id = (isset($_GET['rulesetid'])) ? $_GET['rulesetid'] : $defaultRuleSetID ?>
@@ -10,57 +10,86 @@
 <link href="css/sb-admin.css" rel="stylesheet">
 <!-- Custom JS -->
 <script src="js/rm_rulelist.js"></script>
-<script>var currentRulesetID=<?php echo $current_ruleset_id; ?></script>
+<script>var currentRulesetID =<?php echo $current_ruleset_id; ?></script>
 
 <div id="wrapper">
 
-<?php include_once 'loader_err.php'; ?>
+    <?php include_once 'loader_err.php'; ?>
 
-    <?php include_once 'rm_sidebar.php';?>
+    <?php include_once 'rm_sidebar.php'; ?>
 
     <div id="page-wrapper" style="position:absolute;width:100%;height:100%;">
 
-      <div style="margin-top:10px;">
-        <ol class="breadcrumb" style="margin-bottom:10px;">
-          <li><a href="index.php">Home</a></li>
-          <li>Rule Manager</li>
-          <li id="rule-title-li">Rule List - <?php echo $rule_list[$current_ruleset_id]['RuleName'];?></li>
-        </ol>
-      </div>
+        <div style="margin-top:10px;">
+            <ol class="breadcrumb" style="margin-bottom:10px;">
+                <li><a href="index.php">Home</a></li>
+                <li>Rule Manager</li>
+                <li id="rule-title-li">Rule List - <?php echo $rule_list[$current_ruleset_id]['RuleName']; ?></li>
+                <button class="btn btn-sm-black add_rule_btn" data-toggle="modal" data-target="#addRuleModal"
+                        style="display:inline-block;margin-left:25px;">
+                    Add Rule
+                </button>
+            </ol>
+        </div>
 
-      <span>Select Transcode Rule</span>
-      <button class="btn btn-lg-black add_rule_btn" data-toggle="modal" data-target="#addRuleModal" style="display:inline-block;margin-left:25px;">
-        Add Rule
-      </button>
-      <br>
-      <div id="rule-list-div">
-        <table id="rule-list-table" class="table table-hover">
-          <tbody>
+        <span>Select Transcode Rule</span>
+        <br>
+
+        <div id="rule-list-div">
+            <table id="rule-list-table-main" class="table table-hover">
+                <thead>
+                    <tr><th colspan="2" style="padding-left: 0px;">Main Rule:</th></tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($rule_list as $rule_set_id => $rule) {
+                    if ($rule_list[$rule_set_id]['RuleType'] === 'MainRule') {
+                        $class = ($rule_set_id === (integer)$current_ruleset_id) ? 'info' : '';
+                        echo '<tr style="cursor:pointer;" class="' . $class . '" data-rulesetid="' . $rule_set_id . '" data-rulevar="' . $rule['RuleVar'] . '">
+                      <td style="padding:8px 2px;">' . $rule['RuleName'] . '</td>
+                      <td style="padding:8px 2px;width:75px;">
+                      <button class="btn btn-sm-black edit_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#editRuleModal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                      <button class="btn btn-sm-black clone_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#cloneRuleModal"><i class="fa fa-clone" aria-hidden="true"></i></button>
+                      <button class="btn btn-sm-black del_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#delRuleModal"><i class="fa fa-minus-square-o" aria-hidden="true"></i></button>
+                      </td></tr>';
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+            <br>
+            <table id="rule-list-table-sub" class="table table-hover">
+                <thead>
+                    <tr><th colspan="2" style="padding-left: 0px;">Sub Rule:</th></tr>
+                </thead>
+                <tbody>
+                <?php
+                foreach ($rule_list as $rule_set_id => $rule) {
+                    if ($rule_list[$rule_set_id]['RuleType'] === 'SubRule') {
+                        $class = ($rule_set_id === (integer)$current_ruleset_id) ? 'info' : '';
+                        echo '<tr style="cursor:pointer;" class="' . $class . '" data-rulesetid="' . $rule_set_id . '" data-rulevar="' . $rule['RuleVar'] . '">
+                      <td style="padding:8px 2px;">' . $rule['RuleName'] . '</td>
+                      <td style="padding:8px 2px;width:75px;">
+                      <button class="btn btn-sm-black edit_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#editRuleModal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                      <button class="btn btn-sm-black clone_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#cloneRuleModal"><i class="fa fa-clone" aria-hidden="true"></i></button>
+                      <button class="btn btn-sm-black del_rule_btn" data-rulesetid="' . $rule_set_id . '" data-toggle="modal" data-target="#delRuleModal"><i class="fa fa-minus-square-o" aria-hidden="true"></i></button>
+                      </td></tr>';
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+        <div id="rule-info">
             <?php
-              foreach ($rule_list as $rule_set_id => $rule) {
-                  $class = ($rule_set_id === (integer) $current_ruleset_id) ? 'info' : '';
-                  echo '<tr style="cursor:pointer;" class="'.$class.'" data-rulesetid="'.$rule_set_id.'" data-rulevar="'.$rule['RuleVar'].'">
-                  <td style="padding:8px 2px;">'.$rule['RuleName'].'</td>
-                  <td style="padding:8px 2px;width:75px;">
-                  <button class="btn btn-sm-black edit_rule_btn" data-rulesetid="'.$rule_set_id.'" data-toggle="modal" data-target="#editRuleModal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
-                  <button class="btn btn-sm-black clone_rule_btn" data-rulesetid="'.$rule_set_id.'" data-toggle="modal" data-target="#cloneRuleModal"><i class="fa fa-clone" aria-hidden="true"></i></button>
-                  <button class="btn btn-sm-black del_rule_btn" data-rulesetid="'.$rule_set_id.'" data-toggle="modal" data-target="#delRuleModal"><i class="fa fa-minus-square-o" aria-hidden="true"></i></button>
-                  </td></tr>';
-              }
+            foreach ($rule_list[$current_ruleset_id] as $key => $value) :
+                echo '<span style="font-size:18px">' . $key . ' : ' . $value . '</span><br>';
+            endforeach;
             ?>
-          </tbody>
-        </table>
-      </div>
-      <div id="rule-info">
-        <?php
-        foreach ($rule_list[$current_ruleset_id] as $key => $value) :
-            echo '<span style="font-size:18px">'.$key.' : '.$value.'</span><br>';
-        endforeach;
-        ?>
-      </div>
+        </div>
 
     </div>
 
 </div>
 
-<?php include_once 'footer.php';?>
+<?php include_once 'footer.php'; ?>
